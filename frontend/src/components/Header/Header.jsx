709fd203272d2290f9baceb4ec2bafe7774a6d5b
Anandunique <anandabalagi10220@gmail.com>
2024-07-29 12:39:01 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Satisfy Your Cravings Now</h2>
+                <p>Indulge in our extensive menu, showcasing a tempting selection of dishes made with premium ingredients and exceptional culinary skill. We aim to delight your taste buds and enhance your dining pleasure with every meal. Explore our offerings and experience a new standard in food delivery.</p>
+                <button>See Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
